7328a56d50f56310bf2834727b4f0208cc3a274b
jorge <georgeramirez01@gmail.com>
2019-11-10 20:07:36 -0700

    Fire Ball Enemy

---
+++ GameOff/Assets/Scripts/AI/EnemySpriteRotate.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpriteRotate : MonoBehaviour
+{
+    [SerializeField] float FlipDelay;
+
+    private Transform Player;
+    private bool bussy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        bussy = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.position.x < transform.position.x)
+        {
+            if (FlipDelay <= 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (!bussy)
+            {
+                StartCoroutine("RotateDelayLeft");
+            }
+        }
+        else if (Player.position.x > transform.position.x)
+        {
+            if (FlipDelay <= 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if(!bussy)
+            {
+                StartCoroutine("RotateDelayRight");
+            }
+        }
+    }
+
+    IEnumerator RotateDelayRight()
+    {
+        bussy = true;
+        yield return new WaitForSeconds(FlipDelay);
+        GetComponent<SpriteRenderer>().flipX = true;
+        bussy = false;
+    }
+
+    IEnumerator RotateDelayLeft()
+    {
+        bussy = true;
+        yield return new WaitForSeconds(FlipDelay);
+        GetComponent<SpriteRenderer>().flipX = false;
+        bussy = false;
+
+
+    }
+}
---
+++ GameOff/Assets/Scripts/AI/Flying.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    [SerializeField] public float Speed;
+    [SerializeField] public float TurnSpeed;
+    [SerializeField] public float PlayerYOffSet;
+    [SerializeField] public float ActivationRange;
+    [SerializeField] public float SpotExtendedRange;
+    [SerializeField] public float RangeForAction;
+    [SerializeField] public Transform MovementGuide;
+    [SerializeField] public bool FlowtyFollow;
+    [SerializeField] bool Debugger;
+
+    private Rigidbody2D rb;
+    private float OriginalActivationRange;
+    private Transform Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        OriginalActivationRange = ActivationRange;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Debugger)
+        {
+            if(CloseForAction())
+                Debug.Log("Close Enough");
+
+        }
+    }
+
+    public bool CloseForAction()
+    {
+        if (Vector2.Distance(Player.position, transform.position) < RangeForAction)
+        {
+            return true;
+        }
+        else
+        {
+            Follow();
+            return false;
+        }
+    }
+
+    private void Follow()
+    {
+        if (FlowtyFollow)
+        {
+            if (Vector2.Distance(Player.position, transform.position) < ActivationRange)
+            {
+                Point();
+                ActivationRange = SpotExtendedRange;
+                Point();
+                Vector2 GuideMovemnet = MovementGuide.right * Speed;
+                rb.velocity = GuideMovemnet;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0,0);
+            ActivationRange = SpotExtendedRange;
+            Vector2 Target = new Vector2(Player.position.x, Player.position.y + PlayerYOffSet);
+            transform.position = Vector2.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+        }
+        
+    }
+
+    public void Point()
+    {
+      
+        Vector2 direction = Player.position - MovementGuide.position;
+        direction = new Vector2(direction.x, direction.y + PlayerYOffSet);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        MovementGuide.rotation = Quaternion.Slerp(MovementGuide.rotation, rotation, TurnSpeed * Time.deltaTime);
+    }
+
+
+}
