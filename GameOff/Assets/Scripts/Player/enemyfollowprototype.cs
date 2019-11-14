81877e8f9f3f49aeb31c20ddb2a442d4cb1f1a7b
Katie <katie.louise.gardner@gmail.com>
2019-11-14 13:08:28 +0000

    enemy prototype scripts (ignore these)

---
+++ GameOff/Assets/Scripts/Player/enemyfollowprototype.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyfollowprototype : MonoBehaviour
+{
+    GameObject player;
+    GameObject enemy;
+    public float radius = 1f;
+    public float diveTime = 1f;
+
+    float startTime = 0f;
+    bool inLoop = false;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemy = gameObject;
+        startTime = Time.time;
+    }
+
+    float Dist(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(Mathf.Pow(a.x - b.x,2) + Mathf.Pow(a.y - b.y,2));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.PI / 2;
+        float t = Time.time - startTime;
+        Vector3 playerPos = player.transform.position;
+
+        Vector3 centre = playerPos + Vector3.up*radius; //the centre of the circle the enemy is orbiting around
+        Vector3 direction = centre - enemy.transform.position;
+        float speed = angle * radius / diveTime;
+
+        if (Dist(enemy.transform.position, centre) > radius)
+        {
+            enemy.transform.position += Vector3.Normalize(direction) * Time.deltaTime * speed; //at the moment movement per second time is (Pi*radius/2)/divetime
+        }
+        else if (!inLoop)
+        {
+            Debug.Log("setting positions");
+            //if we are hitting the loop for the first time then we need to make a change to the starting position on the circle
+            float distX = enemy.transform.position.x - playerPos.x;
+            float timing = Mathf.Acos(distX / radius);
+            startTime = Time.time - timing;
+            inLoop = true;
+        }
+        else 
+        {
+            float enemyX = playerPos.x + radius * (Mathf.Cos(angle * (t / diveTime)));
+            float enemyY = playerPos.y + radius - Mathf.Abs(radius * Mathf.Sin(angle * t / diveTime));
+            //alternative dive
+            Vector3 target = enemy.transform.position + Vector3.Normalize(direction) * Time.deltaTime * speed;
+            enemyY = playerPos.y + radius - Mathf.Pow(Mathf.Abs(Mathf.Sin(angle * t / diveTime)), 2) * radius;
+
+            transform.position = new Vector3(enemyX, enemyY);
+        }
+
+    }
+}
---
+++ GameOff/Assets/Scripts/Player/enemyprototwo.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyprototwo : MonoBehaviour
+{ 
+    GameObject player;
+    GameObject enemy;
+    public float radius = 1f;
+    public float diveTime = 1f;
+
+    float startTime = 0f;
+    bool inLoop = false;
+    float timeDamp = 3f;
+    float timeDampTrack;
+    bool damping = true;
+    public float diveForce = 50f;
+
+    Rigidbody2D enemyRB;
+    Vector3 target;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemy = gameObject;
+        startTime = Time.time;
+        enemyRB = enemy.AddComponent<Rigidbody2D>();
+        enemyRB.drag = 1;
+        enemyRB.gravityScale = 0;
+    }
+
+    float Dist(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.PI / 2;
+        float t = Time.time - startTime;
+        float maxDist = Mathf.Sqrt(2) * radius;
+        Vector3 playerPos = player.transform.position;
+
+        Vector3 centre = playerPos + Vector3.up * radius; //the centre of the circle the enemy is orbiting around
+        Vector3 direction = centre - enemy.transform.position;
+        float speed = angle * radius / diveTime;
+
+
+
+        if (Dist(enemy.transform.position, centre) > radius)
+        {
+            enemy.transform.position += Vector3.Normalize(direction) * Time.deltaTime * speed; //at the moment movement per second time is (Pi*radius/2)/divetime
+        }
+        else if (!inLoop)
+        {
+            inLoop = true;
+            target = player.transform.position;
+        }
+        else
+        {
+            direction = target - enemy.transform.position;
+            if(target.y > enemy.transform.position.y)
+            {
+                direction.y *= 1.5f;
+            }
+            
+            //Debug.Log("target "+target);
+            //Debug.Log("direction "+direction);
+            enemyRB.AddForce(Vector3.Normalize(direction) * diveForce*Mathf.Sqrt(Dist(enemy.transform.position,target)));
+            //Debug.Log(Dist(enemy.transform.position, player.transform.position));
+            //if (Dist(enemy.transform.position,player.transform.position)<0.3f)
+            //{
+            //    Debug.Log("Hit player");
+            //    if(enemyRB.velocity.x < 0)
+            //    {
+
+            //        target = player.transform.position + new Vector3(-radius, radius, 0);
+            //        Debug.Log("NEW TARGET "+target);
+            //    }
+            //    else if (enemyRB.velocity.x > 0)
+            //    {
+            //        target = player.transform.position + new Vector3(radius, radius, 0);
+            //    }
+            player.transform.position += Input.GetAxis("Horizontal") * Time.deltaTime * 10f*Vector3.right;    
+            //}
+            //if(Dist(enemyRB.transform.position,player.transform.position+new Vector3(radius,radius,0))<0.3f||Dist(enemy.transform.position,player.transform.position+new Vector3(-radius, radius, 0))<0.3f)
+            //{
+            //    target = player.transform.position;
+            //}
+        }
+
+    }
+}
+
